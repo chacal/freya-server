2e13260cead337ef392e79af1e714b8cee532cc3
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2014-09-14 15:15:08 +0300

    Initial commit

---
+++ true_wind_adjuster.js
@@ -0,0 +1,28 @@
+var readline = require('readline');
+var Bacon = require("baconjs").Bacon;
+
+var AWA;
+var AWS;
+var BTS;
+
+var rl = readline.createInterface({ input: process.stdin, output: "/dev/null" });
+var nmeaMessages = Bacon.fromEventTarget(rl, "line").filter(function(line) { return line.match(/^\$.*\*/) });
+
+var apparentWindsMessages = nmeaMessages.filter(function(line) { return line.match(/.*MWV.*R/) });
+
+apparentWindsMessages.onValue(function(awsLine) {
+  var matches = /MWV,(.*),R,(.*?),/.exec(awsLine);
+  AWA = matches[1];
+  AWS = matches[2];
+});
+
+var boatSpeedMessages = nmeaMessages.filter(function(line) { return line.match(/.*VHW.*/) });
+
+boatSpeedMessages.onValue(function(line) {
+  var matches = /VHW.*M,(.*?),/.exec(line);
+  BTS = matches[1];
+});
+
+Bacon.interval(100).onValue(function() {
+  console.log("AWA: " + AWA + ", AWS: " + AWS + ", BTS: " + BTS)
+});
