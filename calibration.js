1204d2ccff4d85c9df56504189ee1fd6fd47ee92
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2014-09-20 09:56:24 +0300

    Interpolate also between table values when looking up correction
    Earlier version just rounded up to the nearest table value

--- calibration.js
+++ calibration.js
@@ -24,11 +24,20 @@ exports.initialize = function(twsCorrectionTableFile) {
 };
 
 exports.calculateTwsCorrection = function(awa, aws, bts) {
-  tableAwa = roundTo10(awa);
-  tableBts = Math.round(bts);
-  rowIndex = tableAwa / 10;
-  columnIndex = tableBts;
-  return twsCorrectionTable[rowIndex][columnIndex];
+  // Find table rows & columns for surrounding corners
+  tableAwa1 = roundDownTo10(awa)
+  tableAwa2 = roundUpTo10(awa)
+  tableBts1 = Math.floor(bts)
+  tableBts2 = Math.ceil(bts)
+
+  // Surrounding corner table values
+  x1y1 = twsCorrectionTable[tableAwa1 / 10][tableBts1];  // divide AWA by 10 to get the correct row index
+  x1y2 = twsCorrectionTable[tableAwa2 / 10][tableBts1];
+  x2y1 = twsCorrectionTable[tableAwa1 / 10][tableBts2];
+  x2y2 = twsCorrectionTable[tableAwa2 / 10][tableBts2];
+
+  // 2D interpolate between surrounding corner values
+  return roundToPoint1(interpolate(x1y1, x2y1, x1y2, x2y2, bts, tableBts1, tableBts2, awa, tableAwa1, tableAwa2))
 };
 
 
@@ -74,6 +83,32 @@ function calculateAvgSum(table, x, y) {
 }
 
 
+function interpolate(x1y1, x2y1, x1y2, x2y2, x, x1, x2, y, y1, y2) {
+  if(x1 == x2 && y1 == y2) {
+    return x1y1  // Both row & column match to the table
+  } else if(x1 == x2) {
+    return linearInterpolate(y, y1, y2, x1y1, x1y2)  // Column matches table, interpolate row
+  } else if(y1 == y2) {
+    return linearInterpolate(x, x1, x2, x1y1, x2y1)  // Row matches table, interpolate column
+  } else {
+    return bilinearInterpolate(x1y1, x2y1, x1y2, x2y2, x, x1, x2, y, y1, y2)  // Interpolate both row & column
+  }
+}
+
+function bilinearInterpolate(x1y1, x2y1, x1y2, x2y2, x, x1, x2, y, y1, y2) {
+  console.assert(x1 != x2, "Can't do bilinear interpolation with equal x values!")
+  console.assert(y1 != y2, "Can't do bilinear interpolation with equal y values!")
+  r1 = ((x2 - x) / (x2 - x1) * x1y1) + ((x - x1) / (x2 - x1) * x2y1)
+  r2 = ((x2 - x) / (x2 - x1) * x1y2) + ((x - x1) / (x2 - x1) * x2y2)
+  p = ((y2 - y) / (y2 - y1) * r1) + ((y - y1) / (y2 - y1) * r2)
+  return p
+}
+
+function linearInterpolate(x, x1, x2, y1, y2) {
+  return y1 + (y2 - y1) * ((x - x1) / (x2 - x1))
+}
+
+
 
 ///// Helpers /////
 
@@ -85,7 +120,8 @@ function map2DTable(table, func) {
   }
 }
 
-function roundTo10(value) { return Math.round(value / 10) * 10; }
+function roundDownTo10(value) { return Math.floor(value / 10) * 10; }
+function roundUpTo10(value) { return Math.ceil(value / 10) * 10; }
 
 function roundToPoint1(value) { return Math.round(value * 10) / 10; }
 
