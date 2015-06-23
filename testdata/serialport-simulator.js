7933c64b72284fc011326f5fb3f9c141dbc868f9
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-06-23 12:32:36 +0300

    Add SerialportSimulator to enable running without actual serial devices

---
+++ testdata/serialport-simulator.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var path = require('path')
+var util = require('util')
+var Bacon = require("baconjs").Bacon
+var EventEmitter = require('events').EventEmitter
+var _ = require('lodash')
+
+var lines = fs.readFileSync(path.resolve(__dirname + "/Freya_20140913_1638.log")).toString().split('\n')
+
+
+var SerialportSimulator = function(device, options) {
+  console.log("Starting SerialportSimulator:", device)
+
+  this.device = device
+  setTimeout(_.bind(emitOpen, this), 100)
+  setTimeout(_.bind(startSendindData, this), 200)
+}
+util.inherits(SerialportSimulator, EventEmitter)
+
+SerialportSimulator.prototype.write = function(data) {
+  process.stdout.write('Out - ' + this.device + ': ' + data)
+}
+
+
+function emitOpen() { this.emit('open') }
+
+function startSendindData() {
+  Bacon.sequentially(100, lines).onValue(_.bind(function(line) {
+    this.emit('data', line)
+  }, this))
+}
+
+
+
+module.exports = SerialportSimulator
