90e3cdae1e30660d50862e93a8f2a6d1a78399d2
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-04-21 08:49:19 +0300

    Remove nmea_sender (not needed)

--- nmea_sender.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var Bacon = require("baconjs").Bacon;
-
-var lines = fs.readFileSync("testdata/Freya_20140913_1638.log").toString().split('\n');
-
-Bacon.sequentially(2, lines).onValue(function(line) {
-  console.log(line);
-});
