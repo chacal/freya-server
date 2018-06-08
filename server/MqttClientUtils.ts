3f10ae432fb378878a750f307de99edd54075096
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-06-08 21:38:12 +0300

    Implement max MQTT disconnect count
    It might be better to let systemd restart the whole process rather than
    end up in connect/disconnect loop.

--- server/MqttClientUtils.ts
+++ server/MqttClientUtils.ts
@@ -9,15 +9,25 @@ declare module 'baconjs' {
   function fromEvent<E, A>(target: EventTarget|NodeJS.EventEmitter|JQuery, eventName: string, eventTransformer: (t: string, m: string) => A): EventStream<E, A>;
 }
 
+const MAX_DISCONNECT_COUNT = 20
 
 export default {
   connectClient
 }
 
 function connectClient<E>(brokerUrl: string, mqttUsername?: string, mqttPassword?: string): EventStream<E, Client>  {
+  let disconnectCount = 0
+
   const client = mqtt.connect(brokerUrl, { username: mqttUsername, password: mqttPassword })
   client.on('connect', () => console.log('Connected to MQTT server'))
-  client.on('offline', () => console.log('Disconnected from MQTT server'))
+  client.on('offline', () => {
+    disconnectCount++
+    console.log(`Disconnected from MQTT server. Disconnect count: ${disconnectCount}/${MAX_DISCONNECT_COUNT}`)
+    if(disconnectCount === MAX_DISCONNECT_COUNT) {
+      console.log("Exiting due to too many MQTT disconnects..")
+      process.exit(1)
+    }
+  })
   client.on('error', (e) => console.log('MQTT client error', e))
 
   return Bacon.fromEvent<E, Client>(client, 'connect').first()
